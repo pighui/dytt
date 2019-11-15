250259c979f51fadfb57f57fd2bdab62c8a05c38
xuhui <pighui233@163.com>
2019-11-15 14:27:59 +0800

    改用re提取

--- dytt/spiders/movie.py
+++ dytt/spiders/movie.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
+import re
+
 from scrapy import Request
 from scrapy_redis.spiders import RedisSpider
-
-from dytt.items import MovieItem
 
 
 class MovieSpider(RedisSpider):
@@ -13,7 +13,7 @@ class MovieSpider(RedisSpider):
         detail_url_list = response.xpath("//table[@class='tbspan']//a[@class='ulink']/@href").extract()
         for detail_url in detail_url_list:
             full_detail_url = 'https://www.dy2018.com' + detail_url
-            yield Request(url=full_detail_url,callback=self.parse_item,dont_filter=True)
+            yield Request(url=full_detail_url, callback=self.parse_item)
         try:
             next_href = response.xpath("//div[@class='x']/a[1]/@href").extract()[0]
             full_link = 'https://www.dy2018.com/' + next_href
@@ -21,16 +21,30 @@ class MovieSpider(RedisSpider):
         except:
             print('已经是最后一页了')
         else:
-            yield Request(full_link, callback=self.parse, dont_filter=True)
+            yield Request(full_link, callback=self.parse)
 
-
-    def parse_item(self,response):
-        item = MovieItem()
-        item['name'] = response.xpath("//div[@id='Zoom']/p[2]/text()").extract()[0].split("名")[1].strip(" ")
-        item['year'] = response.xpath("//div[@id='Zoom']/p[3]/text()").extract()[0].split("代")[1].strip(" ")
-        item['country'] = response.xpath("//div[@id='Zoom']/p[4]/text()").extract()[0].split("地")[1].strip(" ")
-        item['type'] = response.xpath("//div[@id='Zoom']/p[5]/text()").extract()[0].split("别")[1].strip(" ")
-        item['language'] = response.xpath("//div[@id='Zoom']/p[6]/text()").extract()[0].split("言")[1].strip(" ")
-        item['detail_url'] = response.url
-        item['magnet'] = response.xpath("//div[@id='Zoom']/table[1]/tbody/tr/td/a[1]/text()").extract()[0]
-        yield item
+    def parse_item(self, response):
+        text = ''
+        try:
+            info_list = response.xpath("//div[@id='Zoom']/p/text()").extract()
+        except:
+            info_list = response.xpath("//div[@id='Zoom']/text()").extract()
+        for info in info_list:
+            text += info.replace('\xa0', '').replace('\3000', '').replace(" ", "")
+        pure_text = text.replace("　　", "").replace("　", '')
+        pattern = re.compile('片名(.*?)◎'
+                             '年代(.*?)◎'
+                             '类别(.*?)◎'
+                             '语言(.*?)◎')
+        items = re.findall(pattern, pure_text)
+        if items:
+            name, year, type, language = items[0]
+            detail_url = response.url
+            item = {
+                'name': name,
+                'type': type,
+                'language': language,
+                'year': year,
+                'detail': detail_url
+            }
+            yield item
